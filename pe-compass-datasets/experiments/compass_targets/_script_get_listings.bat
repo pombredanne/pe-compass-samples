53b7829f23971ee6437b41cc039becc0f7f2c1a9
carlos_diaz <carlos_diaz@mcafee.com>
2020-04-12 16:18:36 -0400

    w10-pro-vanilla-dataset

---
+++ pe-compass-datasets/experiments/compass_targets/_script_get_listings.bat
@@ -0,0 +1,14 @@
+pe-compass recurse -d c:\ -x .acm > acms.txt
+pe-compass recurse -d c:\ -x .ax > axs.txt
+pe-compass recurse -d c:\ -x .cpl > cpls.txt
+pe-compass recurse -d c:\ -x .com > coms.txt
+pe-compass recurse -d c:\ -x .dll > dlls.txt
+pe-compass recurse -d c:\ -x .drv > drvs.txt
+pe-compass recurse -d c:\ -x .efi > efis.txt
+pe-compass recurse -d c:\ -x .exe > exes.txt
+pe-compass recurse -d c:\ -x .sys > sys.txt
+pe-compass recurse -d c:\ -x .mui > muis.txt
+pe-compass recurse -d c:\ -x .ocx > ocxs.txt
+pe-compass recurse -d c:\ -x .scr > scrs.txt
+pe-compass recurse -d c:\ -x .tsp > tsps.txt
+
