b2b30a6a5bca96041b65477ef24e40bae2051886
dfirence <carlos.diaz.pro@gmail.com>
2022-11-05 21:37:35 -0400

    rescan content

--- samples/ocx/run_inventory.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-for x in $(ls); do cat $x | jq --arg FILENAME $x '{name: .pe_name, sha2: .ImageHashSignatures.sha2, url: "https://raw.githubusercontent.com/dfirence/pe-compass-samples/master/samples/ocx/\($FILENAME)"}' >> temp.json;done
-cat temp.json | jq -s > inventory.json
--- samples/tsp/run_inventory.sh
+++
@@ -1,4 +0,0 @@
-#! /bin/bash
-
-for x in $(ls); do cat $x | jq --arg FILENAME $x '{name: .pe_name, sha2: .ImageHashSignatures.sha2, url: "https://raw.githubusercontent.com/dfirence/pe-compass-samples/master/samples/tsp/\($FILENAME)"}' >> temp_tsp.json;done
-cat temp_tsp.json | jq -s > inventory_tsp.json
